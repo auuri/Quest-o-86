3fbb11241915094203da0fe82eb68a2adcff5e1a
auuri <145621454+auuri@users.noreply.github.com>
2024-04-17 20:17:44 -0300

    Atualizar o 23questao.java

--- 23questao.java
+++ 23questao.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Produto {
+public class 23questao.java {
     private String nome;
     private double preco;
     private int quantidade;
