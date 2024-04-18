c0de1894030393e069ba2435f9c12b7f9d898cc1
auuri <145621454+auuri@users.noreply.github.com>
2024-04-17 23:12:29 -0300

    Atualizar o 1questao.java

---
+++ 1questao.java
@@ -0,0 +1,25 @@
+package edu.br.uniaeso;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class 1questao.java 
+{
+    public static void main( String[] args ){
+    String meuarquivo = "meuarquivo.txt";
+    try{
+        FileWriter
+        arquivoWriter = new
+        FileWriter (meuarquivo);
+        PrintWriter escrever = new PrintWriter(meuarquivo);
+        escrever.println("Olá, mundo!");
+        escrever.close();
+        arquivoWriter.close();
+        System.out.println(("Arquivo criado com sucesso."));
+    } catch (IOException e) {
+        System.out.println(("Ocorreu um erro!!"));
+        e.printStackTrace();
+     }
+    }
+}
